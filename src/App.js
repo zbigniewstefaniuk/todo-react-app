a02afe3e76b83c73822be891a761fd1e32507cad
zbigniewstefaniuk <zbyszek791@hotmail.com>
2020-12-09 23:02:00 +0100

    Initial commit with react boilerplate

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="Todo-App">
+        <TodoList />
+      </header>
+    </div>
+  );
+}
+
+export default App;
