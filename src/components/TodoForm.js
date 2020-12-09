c5c8f218b38b3445a1ebe31c5bb52c86efb13f21
zbigniewstefaniuk <zbyszek791@hotmail.com>
2020-12-09 23:08:16 +0100

    added usestate, useeffect, useref and input form

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 function TodoForm(props) {
+    const [input, setInput] = useState('')
+
+    const inputRef = useRef(null)
+
+    useEffect(() => {
+        inputRef.current.focus()
+    })
+
+    const handleChange = e => {
+        setInput(e.target.value)
+    }
+
+    const handleSubmit = evenet => {
+        evenet.preventDefault()
+
+        props.onSubmit({
+            id: Math.floor(Math.random() * 10000),
+            text: input
+        })
+
+        setInput('')
+    }
 
     return (
         <div>
-
+            <form className='todo-form' onSubmit={handleSubmit}>
+                <input type='text'
+                    placeholder='Anything you wan to remember?'
+                    value={input}
+                    name='text'
+                    className='todo-input'
+                    onChange={handleChange} 
+                    ref={inputRef}
+                    />
+                <button className='todo-button'>Add Todo</button>
+            </form>
         </div>
     )
 }
