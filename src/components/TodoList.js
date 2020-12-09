7984315e07316a2d022c5b83180fd91459a58a77
zbigniewstefaniuk <zbyszek791@hotmail.com>
2020-12-09 23:17:24 +0100

    added complete todo on click - linebreak

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -17,12 +17,22 @@ function TodoList() {
         setTodos(newTodos)
     }
 
+    const completeTodo = id => {
+        let updatedTodos = todos.map(todo => {
+            if (todo.id === id) {
+                todo.isComplete = !todo.isComplete
+            }
+            return todo
+        })
+        setTodos(updatedTodos)
+    }
     return (
         <div>
             <h1>What's the plan for today?</h1>
             <TodoForm onSubmit={addTodo} />
             <Todo
-                todos={todos}/>
+                todos={todos}
+                completeTodo={completeTodo}/>
         </div>
     )
 }
